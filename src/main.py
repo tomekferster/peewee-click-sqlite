2bd38615109bf9b6d48bc507d110cef963fd0888
tomekferster <tomasz.ferster@gmail.com>
2020-08-07 11:48:59 -0400

    added CLI, created more tables to simplify data readability and querying, added docstrings and comments

---
+++ src/main.py
@@ -0,0 +1,244 @@
+import json
+import sqlite3
+import datetime
+import requests
+from peewee import *
+
+db = SqliteDatabase('persons.sqlite3')
+
+
+class BaseModel(Model):
+    """
+    Every other class inherits the db setting from this 'BaseModel' class
+    """
+    class Meta:
+        database = db
+
+
+class Person(BaseModel):
+    gender = CharField()
+    email = CharField()
+    phone = CharField()
+    cell = CharField()
+    nat = CharField()
+
+    def clean_number(self, number):
+        return ''.join(char for char in number if char.isalnum())
+
+
+class Name(BaseModel):
+    person = ForeignKeyField(Person, backref="name")
+    title = CharField()
+    first = CharField()
+    last = CharField()
+
+
+class Location(BaseModel):
+    person = ForeignKeyField(Person, backref="location")
+    city = CharField()
+    state = CharField()
+    postcode = CharField()
+
+
+class Street(BaseModel):
+    location = ForeignKeyField(Location, backref="street")
+    number = IntegerField()
+    name = CharField()
+
+
+class Coordinates(BaseModel):
+    location = ForeignKeyField(Location, backref="coordinates")
+    latitude = CharField()
+    longitude = CharField()
+
+
+class Timezone(BaseModel):
+    location = ForeignKeyField(Location, backref="timezone")
+    offset = CharField()
+    description = CharField()
+
+
+class Login(BaseModel):
+    person = ForeignKeyField(Person, backref="login")
+    uuid = CharField()
+    username = CharField()
+    password = CharField()
+    salt = CharField()
+    md5 = CharField()
+    sha1 = CharField()
+    sha256 = CharField()
+
+
+class Dob(BaseModel):
+    person = ForeignKeyField(Person, backref="dob")
+    date = CharField()
+    age = IntegerField()
+    count_dob = IntegerField()
+
+    def count_days(self):
+        """
+        Method for counting how many days has left to the next birthday
+        """
+        flag = 0
+        now = datetime.datetime.now()
+        dob = datetime.datetime.strptime(self.date, "%Y-%m-%dT%H:%M:%S.%fZ")
+        if now.month > dob.month or (now.month == dob.month and now.day > dob.day):
+            flag = 1
+        try:
+            dob_now = datetime.datetime(now.year + flag, dob.month, dob.day)
+        except ValueError:  # leap year
+            dob_now = datetime.datetime(now.year + flag, dob.month + 1, 1)  
+        return int((dob_now - now).days) + 1
+
+
+class Registered(BaseModel):
+    person = ForeignKeyField(Person, backref="dob")
+    date = CharField()
+    age = IntegerField()
+
+
+class Id(BaseModel):
+    person = ForeignKeyField(Person, backref="id_")
+    name = CharField()
+    value = CharField(null=True)
+    
+
+
+def json_or_API():
+    """
+    Function for loading the data from json file or API
+    """
+    while True:
+        decision = input("Press:\n1 - to load the data from 'persons.json' file\n2 - to load the data from API:\n")
+        if decision == '1' or decision == '2':
+            break
+
+    # OPEN JSON FILE AND POPULATE THE TABLES
+    if decision == '1':
+        with open('persons.json', encoding='utf8') as persons_json:
+            persons = json.load(persons_json)
+
+    # OPEN API URL AND POPULATE THE TABLES
+    elif decision == '2':
+        while True:
+            try:
+                quantity = int(input('How many random people would you like to put in the DB?: '))
+                break
+            except:
+                print("It has to be a number!")
+                continue
+
+        url = f'https://randomuser.me/api/?results={quantity}&exc=picture'
+        r = requests.get(url)
+        persons = json.loads(r.content, encoding='utf8')
+    
+    return persons
+
+
+def create_populate_table():
+    """
+    Function for creating and populating the database
+    """
+
+    # CONNECT TO THE SQLITE DB AND CREATE TABLES
+    db.connect()
+    db.create_tables([Person, Name, Location, Street, Coordinates, Timezone, Login, Dob, Registered, Id])
+
+    # CHOOSE IF YOU WANT TO LOAD THE DATA FROM FILE OR API
+    persons = json_or_API()
+
+    print("Wait until you get a notification!")
+
+    # POPULATE THE DB
+    for person in persons['results']:
+        p = Person(
+                    gender=person['gender'],
+                    email=person['email'],
+                    phone=person['phone'],
+                    cell=person['cell'],
+                    nat=person['nat']
+                    )
+        p.phone = p.clean_number(p.phone)
+        p.cell = p.clean_number(p.cell)
+
+        n = Name(
+                person=p,
+                title=person['name']['title'],
+                first=person['name']['first'],
+                last=person['name']['last']
+                )
+
+        l = Location(
+                    person=p,
+                    city=person['location']['city'],
+                    state=person['location']['state'],
+                    postcode=person['location']['postcode']
+                    )
+
+        s = Street(
+                    location=l,
+                    number=person['location']['street']['number'],
+                    name=person['location']['street']['name']
+                    )
+
+        c = Coordinates(
+                        location=l,
+                        latitude=person['location']['coordinates']['latitude'],
+                        longitude=person['location']['coordinates']['longitude']
+                        )
+
+        t = Timezone(
+                    location=l,
+                    offset=person['location']['timezone']['offset'],
+                    description=person['location']['timezone']['description']
+                    )
+        
+        log = Login(
+                    person=p,
+                    uuid=person['login']['uuid'],
+                    username=person['login']['username'],
+                    password=person['login']['password'],
+                    salt=person['login']['salt'],
+                    md5=person['login']['md5'],
+                    sha1=person['login']['sha1'],
+                    sha256=person['login']['sha256']
+                    )
+
+        d = Dob(
+                person=p,
+                date=person['dob']['date'],
+                age=person['dob']['age']
+                )
+        d.count_dob = d.count_days()
+        
+        r = Registered(
+                        person=p,
+                        date=person['registered']['date'],
+                        age=person['registered']['age']
+                        )
+        
+        i = Id(
+                person=p,
+                name=person['id']['name'],
+                value=person['id']['value']
+                )
+
+        # SAVE DATA
+        p.save()
+        n.save()
+        l.save()
+        s.save()
+        c.save()
+        t.save()
+        log.save()
+        d.save()
+        r.save()
+        i.save()
+
+    # CLOSE THE DB
+    db.close()
+    print("DONE!")
+
+if __name__ == "__main__":
+    
+    create_populate_table()
---
+++ src/script.py
@@ -0,0 +1,178 @@
+import click
+import re
+import string
+from datetime import date, datetime
+from main import (
+                Dob,
+                Location, 
+                Login,
+                Name,
+                Person
+)
+           
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('gender', default='all')
+def average_age(gender):
+    """
+    Specify gender to get the average age
+    """
+    count_all = 0
+    age_all = 0
+
+    if gender == 'female' or gender == 'male':
+        dobs = Dob.select().join(Person).where(Person.gender == gender)
+    else:
+        dobs = Dob.select()
+    
+    for dob in dobs:
+        age_all += dob.age
+    count_all = dobs.count()
+    click.echo("Average age for {} is {:.1f} years".format(gender, age_all/count_all))
+
+
+@cli.command()
+@click.argument('number', default=1)
+def most_common_cities(number):
+    """
+    Specify the number to get the most common cities
+    """
+    city_count = {}
+
+    locations = Location.select()
+    for location in locations:
+        if location.city in city_count.keys():
+            city_count[location.city] += 1
+        else:
+            city_count[location.city] = 1
+
+    most_popular = sorted(city_count.items(), key=lambda x: x[1], reverse=True)
+
+    for index in range(number):
+        city, count = most_popular[index]
+        click.echo("{} - {}".format(city, count))
+
+
+@cli.command()
+@click.argument('number', default=1)
+def most_common_passwords(number):
+    """
+    Specify the number to get the most common passwords
+    """
+    password_count = {}
+
+    logins = Login.select()
+    for login in logins:
+        if login.password in password_count.keys():
+            password_count[login.password] += 1
+        else:
+            password_count[login.password] = 1
+
+    most_popular = sorted(password_count.items(), key=lambda x: x[1], reverse=True)
+
+    for index in range(number):
+        password, count = most_popular[index]
+        click.echo("{} - {}".format(password, count))
+
+
+@cli.command()
+def most_secure_password():
+    """
+    Get the best rated password
+    """
+    best_score = 0
+    best_password = ''
+    special_chars = set(string.punctuation.replace("_", ""))
+    
+    logins = Login.select()
+    for login in logins:
+        score = 0
+        password = login.password
+        if re.search(r'[a-z]', password):
+            score += 1
+        if re.search(r'[A-Z]', password):
+            score += 2
+        if re.search(r'[1-9]', password):
+            score += 1
+        if len(password) > 7:
+            score += 5
+        if any(char in special_chars for char in password):
+            score += 3
+        
+        if score > best_score:
+            best_score = score
+            best_password = password
+    click.echo("Best password: {}, score: {}".format(best_password, best_score))
+
+
+def get_date(date_input):
+    """
+    Function for dates evaluation, it returns 'date' type, if given correctly.
+    Used only by 'people_by_dob_range' cli command
+    """
+    while True:
+        try:
+            dt = date(int(date_input[:4]), int(date_input[5:7]), int(date_input[8:10]))
+            break
+        except:
+            print("Wrong value, remember about the right format! (YYYY-MM-DD)")
+            date_input = input("Provide the date (YYYY-MM-DD): ")
+            continue
+    return dt
+
+@cli.command()
+def people_by_dob_range():
+    """
+    Provide the date range and get all the people within this range
+    """
+    flag_change = ''
+    flag_found = False
+    date1 = click.prompt("Provide the first date (YYYY-MM-DD)", value_proc=get_date)
+    date2 = click.prompt("Provide the second date (YYYY-MM-DD)", value_proc=get_date)
+
+    # if the first date is greater than the second one, change the places of these dates
+    if date1 > date2:
+        flag_change = date2
+        date2 = date1
+        date1 = flag_change
+    
+    persons = Person.select()
+    for person in persons:
+        dob = Dob.select().join(Person).where(Person.id == person.id).get()
+        name = Name.select().join(Person).where(Person.id == person.id).get()
+        dt = datetime.strptime(dob.date, "%Y-%m-%dT%H:%M:%S.%fZ")
+        dob_new = date(dt.year, dt.month, dt.day)
+
+        if date1 <= dob_new <= date2:
+            result = name.first + ' ' + name.last + ' - ' + str(dob_new)
+            click.echo(result)
+            flag_found = True
+    if not flag_found:
+        click.echo("No results!")
+
+
+@cli.command()
+@click.argument('gender')
+def percent_by_gender(gender):
+    """
+    Get the percentage of the specified gender in the whole community
+    """
+    count_all = 0
+    count_choice = 0
+
+    if gender == 'female' or gender == 'male':
+        count_all = Person.select().count()
+        count_choice = Person.select().where(Person.gender == gender).count()
+        click.echo('Percent of {}: {:.1f}%'.format(gender, count_choice * 100/count_all))
+    else:
+        click.echo('Wrong gender, try again!')
+        
+
+if __name__ == "__main__":
+    cli()
